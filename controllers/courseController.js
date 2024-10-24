96521962d2b2c7aef8b718f844c706c05fa70ec0
mamee13 <mamaruyirga1394@gmail.com>
2024-10-24 21:10:12 +0300

    configuring multer to add photos and videos

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -3,6 +3,13 @@ const Course = require('../models/courseModel');
 const AppError = require('../utils/AppError');
 
 exports.createCourse = async (req, res, next) => {
+    let updates = { ...req.body }; // Store the updates from req.body
+
+    if (req.fileName) {
+        // If there is a file, include the filename in the updates
+        const photoPath = path.join('uploads', req.fileName);
+        updates.videos = req.file; // Assuming you want to save the photo's filename
+    }
     const data = await Course.create(req.body);
     res.status(201).json({
         status: 'success',
